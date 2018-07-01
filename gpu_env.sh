46d7592e68ec94e8f8ccfb5b6db72747b0302c9f
Javier Duarte <jduarte1@fnal.gov>
2018-06-30 21:05:29 -0700

    caltech cluster

--- gpu_env.sh
+++ gpu_env.sh
@@ -7,3 +7,8 @@ source gpu_env.sh
 cd $THISDIR
 export PYTHONPATH=`pwd`/modules:$PYTHONPATH
 export DEEPJET=`pwd`
+
+# for caltech cluster:
+rm -rf $HOME/.nv
+export CUDA_CACHE_PATH=/tmp/$USER/cuda/
+mkdir -p $CUDA_CACHE_PATH
