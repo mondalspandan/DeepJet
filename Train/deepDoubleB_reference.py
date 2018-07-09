7016adeb411351734003bc79d21200e4b726c46e
Javier Duarte <jduarte1@fnal.gov>
2018-07-09 12:20:37 -0700

    smaller batch

--- Train/deepDoubleB_reference.py
+++ Train/deepDoubleB_reference.py
@@ -4,12 +4,8 @@ from DeepJetCore.training.training_base import training_base
 from Losses import loss_NLL, loss_meansquared   
 from DeepJetCore.modeltools import fixLayersContaining,printLayerInfosAndWeights
 
-trainDataCollection_60Track = '/data/shared/BumbleB/convert_20180401_ak8_deepDoubleB_db_cpf_sv_reduced_train_val/dataCollection.dc'
-testDataCollection_60Track = '/data/shared/BumbleB/convert_20180401_ak8_deepDoubleB_db_cpf_sv_reduced_test/dataCollection.dc'
-trainDir = "train_2018Samples_60Track/"
-
 #also does all the parsing
-train=training_base(testrun=False,renewtokens=False)
+train=training_base(testrun=True,renewtokens=False)
 
 if not train.modelSet():
     from models import model_deepDoubleBReference as trainingModel
@@ -22,12 +18,12 @@ if not train.modelSet():
                        loss_weights=[1.])
     
     model,history,callbacks = train.trainModel(nepochs=200,
-                                               batchsize=1024,
+                                               batchsize=128,
                                                stop_patience=50,
                                                lr_factor=0.5,
                                                lr_patience=10,
                                                lr_epsilon=0.00000001,
                                                lr_cooldown=2,
                                                lr_minimum=0.00000001,
-                                               maxqsize=10)
+                                               maxqsize=5)
     
